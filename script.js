7713204652194460b2274f309071cb881b477cb3
Mali Gaurav <maligaurav947@gmail.com>
2022-09-11 09:21:23 +0530

    js implement

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+const cardSection = document.getElementById("cardSection");
 const navLinks = document.getElementById("navLinks");
 const closeBtn = document.getElementById("cross");
-const menu = document.getElementById("menu");
-menu.addEventListener("click", () => {
-  menu.style.display = "none";
+const openBTN = document.getElementById("menu");
+const overlayCross = document.getElementById("overlayCross");
+const overLay = document.getElementById("overLay");
+let n = 1;
+
+overlayCross.addEventListener("click", () => {
+  overLay.style.display = "none";
+});
+
+openBTN.addEventListener("click", () => {
+  openBTN.style.display = "none";
   navLinks.style.display = "flex";
   closeBtn.style.display = "grid";
 });
 closeBtn.addEventListener("click", () => {
-  menu.style.display = "grid";
+  openBTN.style.display = "grid";
   navLinks.style.display = "none";
   closeBtn.style.display = "none";
 });
+function addToCard() {
+  cardSection.innerText = n++;
+}
+function openOverlay() {
+  overLay.style.display = "flex";
+}
